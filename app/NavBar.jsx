a1740f35f2eafad43d329a79a6a7afc87dbf77e3
dalienst <oduordalienst@gmail.com>
2024-06-04 13:03:55 +0300

    get user object

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -5,22 +5,30 @@ import { signIn, signOut, useSession } from "next-auth/react";
 function NavBar() {
   const { data: session } = useSession();
 
-  console.log({ session });
+  console.log({ ...session?.user });
 
   return (
     <>
       <nav className="navbar bg-white shadow p-3">
         <div className="container">
-          <Link className="navbar-brand fw-bold text-uppercase" href="/">
-            AuthSimplified
-          </Link>
+          {session?.user ? (
+            <>
+              <Link className="navbar-brand fw-bold text-uppercase" href="/">
+                {session?.user?.first_name}
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link className="navbar-brand fw-bold text-uppercase" href="/">
+                AuthSimplified
+              </Link>
+            </>
+          )}
 
           {session?.user ? (
             <>
               <div className="d-flex align-items-center justify-content-center gap-3">
-                <p className="text-success mb-0">
-                  In Session : {session.user.email}
-                </p>
+                <p className="text-success mb-0">In Session</p>
                 <Link className="btn btn-outline-info btn-sm" href="/admin">
                   Admin
                 </Link>
