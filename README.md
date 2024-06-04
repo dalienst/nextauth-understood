5201de79dfeffd97c850c14e5ed9bd9eb3984078
dalienst <oduordalienst@gmail.com>
2024-06-04 10:20:22 +0300

    complete authentication

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NextJS and NextAuth Authentication
+
+1. NextJS 14 - App Router
+2. NextAuth Package
+3. Custom Signin page
+4. Protected Routes
--- app/admin/panel/page.jsx
+++ app/admin/panel/page.jsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <>
       <div className="container py-5">
-        <h2 className="text-bg-success w-100 p-2 border rounded text-center">
+        <h2 className="text-bg-info w-100 p-2 border rounded text-center">
           This is the admin panel page
         </h2>
       </div>
